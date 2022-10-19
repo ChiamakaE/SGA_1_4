390919ccf8223a0ca7bc9cc26709ba7d3707090b
chiamaka <eboaguchiamaka@gmail.com>
2022-10-19 13:03:39 +0100

    This is my first commit

---
+++ week1_day1/hello_world.py
@@ -0,0 +1,36 @@
+# print("hello world")
+# print ("amaka's world")
+# multiline_string = '''
+# What
+# is 
+# going
+# on
+# here
+# '''
+# print(multiline_string)
+
+
+
+# my_dict = {"name_1":"Amaka", "age":40, "hobbies":["reading","singing","coding"],"my_multiline": multiline_string}
+# print(my_dict)
+
+# print(my_dict["age"])
+
+# print(my_dict["my_multiline"])
+
+# my_list = ["apple","orange","banana","pear"]
+# print(my_list)
+# print(my_list[2])
+
+# # muting values in a list
+# my_list[2] = "mangoes"
+# print(my_list[2])
+
+my_tuple = ("table","chair","shoe")
+print(my_tuple)
+print(my_tuple[2])
+
+# my_tuple[2] = "bed"
+# print(my_tuple[2])
+
+print(type(my_tuple))
---
+++ week1_day2/control_flow.py
@@ -0,0 +1,19 @@
+#if statement pratice
+x = int(input("Please input your grade:"))
+if type(x) == int:
+
+    if x > 70:
+        print("A")
+    elif x >= 60 and x <= 69:
+        print("B")
+    elif x >= 50 and x <= 59:
+        print("C")
+    elif x >=40  and x <= 49:
+        print("D")
+    else:
+        print("Failed")
+
+else:
+    print("our program only accepts integer values")
+
+print("I will execute either way")
